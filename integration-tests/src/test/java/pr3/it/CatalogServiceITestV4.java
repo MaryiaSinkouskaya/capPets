e4ac434ee25994cb8b2ac5c7e9d1332f3f6ecaab
Maryia Sinkouskaya <maryia.sinkouskaya@leverx.com>
2021-04-23 15:48:20 +0300

    Integration tests: testReadPets, testChangeUser

--- integration-tests/src/test/java/pr3/it/CatalogServiceITestV4.java
+++ integration-tests/src/test/java/pr3/it/CatalogServiceITestV4.java
@@ -1,9 +1,5 @@
 package pr3.it;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +10,43 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pr3.Application;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
 public class CatalogServiceITestV4 {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void testReadBooks() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/odata/v4/CatalogService/Books")).andExpect(status().isOk())
-				.andExpect(jsonPath("$.value[0].title").value(containsString("Wuthering Heights")))
-				.andExpect(jsonPath("$.value[0].stock").value(100))
-				.andExpect(jsonPath("$.value[1].title").value(containsString("Jane Eyre (discounted)")))
-				.andExpect(jsonPath("$.value[1].stock").value(500));
-	}
+    @Test
+    public void testReadPets() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/odata/v4/CatalogService/Pets"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.value[0].ID").value(1))
+                .andExpect(jsonPath("$.value[0].name").value("catto"))
+                .andExpect(jsonPath("$.value[1].ID").value(2))
+                .andExpect(jsonPath("$.value[1].name").value("doggo"));
+    }
+
+    @Test
+    public void testChangeUser() throws Exception {
+        int petId = 2;
+        int userId = 2;
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/odata/v4/CatalogService/Pets(" + petId + ")/CatalogService.changeUser")
+                .contentType(APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"user\": {\n" +
+                        "        \"ID\": " + userId + "\n" +
+                        "    }\n" +
+                        "}")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.user_ID").value(userId));
+    }
 }
