ee8b35aeb4d1167850b8f089b311c78a2e84aa31
Maryia Sinkouskaya <maryia.sinkouskaya@leverx.com>
2021-04-30 11:05:52 +0300

    added tests for validator

---
+++ srv/src/test/java/pr3/groovy/validators/CatalogServiceValidatorTest.groovy
@@ -0,0 +1,40 @@
+package pr3.groovy.validators
+
+import com.sap.cds.services.ServiceException
+import pr3.services.UserService
+import pr3.validators.CatalogServiceValidator
+import spock.lang.Specification
+
+import static pr3.utils.TestUtils.validId
+
+class CatalogServiceValidatorTest extends Specification {
+
+    def userService = Mock(UserService)
+    def catalogServiceValidator = new CatalogServiceValidator(userService)
+
+    //todo: rename all def's
+    def "checkAttaching should throw ServiceException"() {
+
+        def id = validId();
+
+        when:
+        catalogServiceValidator.checkAttaching(id, id)
+
+        then:
+        thrown(ServiceException)
+    }
+
+    def "checkUserExistence should throw ServiceException"() {
+
+        Integer userId = validId();
+        userService.getUser(userId) >> {
+            throw new ServiceException("User not found or doesn't exist")
+        }
+
+        when:
+        catalogServiceValidator.checkUserExistence(userId);
+
+        then:
+        thrown(ServiceException)
+    }
+}
